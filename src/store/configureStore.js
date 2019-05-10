4365a9c28f1691324658e9f5276a85d01de6f13c
matt <wilderson.matt@gmail.com>
2019-05-10 20:48:23 +1200

    remove reducer

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -4,10 +4,6 @@ import { createStore, combineReducers } from 'redux';
 
 export default () => {
   const store = createStore(
-    // combineReducers({
-    //   // expenses: expensesReducer,
-    //   // filters: filtersReducer
-    // }),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
 
