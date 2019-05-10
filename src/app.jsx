0b2873ced74b7cc0ede5ec3f1720aa5e5c068e5b
matt <wilderson.matt@gmail.com>
2019-05-10 19:27:18 +1200

    fix deployment

--- src/app.jsx
+++ src/app.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import AppRouter from "./routers/AppRouter";
 import configureStore from "./store/configureStore";
-import { setTextFilter } from "./actions/filters";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 import registerServiceWorker from "./components/registerServiceWorker";
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,30 @@
 const path = require("path");
 
 module.exports = {
-  entry: { index: ["babel-polyfill", "./src/app.js"] },
+  entry: { index: ["babel-polyfill", "./src/app.jsx"] },
   output: {
     path: path.join(__dirname, "public"),
     filename: "bundle.js"
   },
   module: {
     rules: [
-      {
-        loader: "babel-loader",
-        test: /\.js$/,
-        exclude: /node_modules/
-      },
+      { 
+        test: /\.jsx?$/,         // Match both .js and .jsx files
+        exclude: /node_modules/, 
+        loader: "babel-loader", 
+        query:
+          {
+            presets:['es2015',"stage-0",'react']
+          }
+    },
       {
         test: /\.s?css$/,
         use: ["style-loader", "css-loader", "sass-loader"]
-      }
+      },
     ]
   },
   devtool: "cheap-module-eval-source-map",
   devServer: {
-    contentBase: path.join(__dirname, "public"),
-    historyApiFallback: true
+    contentBase: path.join(__dirname, "public")
   }
 };
