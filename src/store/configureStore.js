86b34d7b3a8e237c649cf2eae23ce0664475cb28
matt <wilderson.matt@gmail.com>
2019-05-10 20:18:45 +1200

    add env

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -4,10 +4,10 @@ import { createStore, combineReducers } from 'redux';
 
 export default () => {
   const store = createStore(
-    combineReducers({
-      // expenses: expensesReducer,
-      // filters: filtersReducer
-    }),
+    // combineReducers({
+    //   // expenses: expensesReducer,
+    //   // filters: filtersReducer
+    // }),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
 
